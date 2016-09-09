55304abf09516d3c33756b839a15dc3b04498cf4
Labib Fawaz <lfawaz@inflocalusers-MacBook-Pro-2.local>
2016-09-09 15:56:48 -0400

    run neural network implement logistic regression

--- All State Purchase Prediction Challenge/pythonASPPC/neural_net.py
+++ All State Purchase Prediction Challenge/pythonASPPC/neural_net.py
@@ -5,13 +5,21 @@
 
 from sknn.mlp import Classifier, Layer
 from  sklearn.cross_validation import train_test_split
-from utils import grid_search,load_clean_data
+from utils import load_clean_data
 import datetime
+from sklearn import preprocessing
 
 str(datetime.datetime.now())
-def test_mlp_classifier(X,y,layers=[Layer("Rectifier",units=10),Layer('Softmax')],learning_rate=0.02,n_iter=1):
+def test_mlp_classifier(data,layers=[Layer("Rectifier",units=10),Layer('Softmax')],learning_rate=0.02,n_iter=1,scale=1):
     
-   
+    #preprossing data if necessary 
+    X_raw, y = data
+    
+    if scale == 1:
+        X = preprocessing.scale(X_raw)
+    else:
+        X = X_raw
+
     #since our test set is not labeled I am using the training data provided for train, validation and test
     print ("Create, train, test, validation_sets")
     X_train_valid, X_test, y_train_valid, y_test = train_test_split(X,y, test_size =0.2,random_state=42)
@@ -38,26 +46,37 @@ def test_mlp_classifier(X,y,layers=[Layer("Rectifier",units=10),Layer('Softmax')
 # In[18]:
 
 def main():
-    X,y = load_clean_data()
+    data = load_clean_data()
     
-    network_configs = [{"layers":"[Layer('Rectifier',units=20),Layer('Softmax')]","learning_rate": 0.02,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=40),Layer('Softmax')]","learning_rate": 0.03,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=60),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=80),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=100),Layer('Softmax')]","learning_rate": 0.001,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=20),Layer('Rectifier',units=20),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=40),Layer('Rectifier',units=40),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=60),Layer('Rectifier',units=60),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=80),Layer('Rectifier',units=80),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
-              {"layers":"[Layer('Rectifier',units=100),Layer('Rectifier',units=100),Layer('Softmax')]","learning_rate": 0.001,"n_inter": 100}]
+    network_configs = [{"layers":"[Layer('Rectifier',units=20,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.02,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=40,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.03,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=60,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=80,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=100,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.001,"n_inter":100},
+              {"layers":"[Layer('Rectifier',units=20,weight_decay=0.0001,dropout=0.1),Layer('Rectifier',units=20,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=40,weight_decay=0.0001,dropout=0.1),Layer('Rectifier',units=40,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=60,weight_decay=0.0001,dropout=0.1),Layer('Rectifier',units=60,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=80,weight_decay=0.0001,dropout=0.1),Layer('Rectifier',units=80,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.01,"n_inter": 100},
+              {"layers":"[Layer('Rectifier',units=100,weight_decay=0.0001,dropout=0.1),Layer('Rectifier',units=100,weight_decay=0.0001,dropout=0.1),Layer('Softmax')]","learning_rate": 0.001,"n_inter": 100}]
     
     for config in network_configs:       
-        score,layers,learning_rate,n_iter = test_mlp_classifier(X=X,y=y,layers=eval(config.get('layers')),learning_rate=config.get('learning_rate'),n_iter=config.get('n_inter'))
-        line = ("score: %s, layers: %s, learning_rate: %s, n_iter %s" %(score, layers, learning_rate, n_iter))
         f = open('neural_net_score','a')
-        f.write("\n start:  %s | " %(str(datetime.datetime.now())))
+
+        start = "\n start:  %s | " %(str(datetime.datetime.now()))
+        print('start time....'), start
+        
+        f.write(start)
+        
+        layers = config.get('layers')
+        learning_rate = config.get('learning_rate')
+        n_iter = config.get('n_inter')
+        print "Configs...",layers,
+        score,layers,learning_rate,n_iter = test_mlp_classifier(data,layers=eval(layers),learning_rate=learning_rate,n_iter=n_iter,scale=1)
+        line = ("score: %s, layers: %s, learning_rate: %s, n_iter %s" %(score, layers, learning_rate, n_iter))
+        
         f.write(line)
-        f.write(" | End %s" %(str(datetime.datetime.now())))
+        End = " | End %s" %(str(datetime.datetime.now()))
+        f.write(End)
         f.close()
 
 
